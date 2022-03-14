b2946f863cbdc4a69fcbd8391393aae723f77f54
karam  mustafa <karam2mustafa@gmail.com>
2022-03-14 20:34:55 +0200

    wip

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 
 
+use App\Models\Post;
 use App\Models\User;
 
 /**
@@ -54,19 +55,90 @@ class PostTest extends BaseFeatureTest
     }
 
     /**
-     * test create a new post and get validation error, with testing for message.
+     * test create a new post.
      *
      * @author karam mustafa
      */
     public function test_create_new_post()
     {
-        $this->withoutExceptionHandling();
+        // create a new user.
         $user = User::factory()->create();
 
+        // post new request to create a post.
         $this->postJson(static::POST_API,[
             'user_id' => $user->id,
             'description' => 'dummy description',
             'title' => 'dummy title',
         ])->assertStatus(static::$SUCCESS_RESPONSE);
+
+        // check database records.
+        $this->assertDatabaseHas('posts', [
+            'user_id' => $user->id,
+            'description' => 'dummy description',
+            'title' => 'dummy title',
+        ]);
+    }
+
+    /**
+     * test create a new post.
+     *
+     * @author karam mustafa
+     */
+    public function test_edit_custom_post()
+    {
+        // create new post
+        $user = User::factory()->create();
+        $this->postJson(static::POST_API,[
+            'user_id' => $user->id,
+            'description' => 'dummy description',
+            'title' => 'dummy title',
+        ])->assertStatus(static::$SUCCESS_RESPONSE);
+
+        // check database records.
+        $this->assertDatabaseHas('posts', [
+            'user_id' => $user->id,
+            'description' => 'dummy description',
+            'title' => 'dummy title',
+        ]);
+
+        $post = Post::first();
+
+        //edit post
+        $this->putJson(static::POST_API."/{$post->id}",[
+            'user_id' => $user->id,
+            'description' => 'dummy description updated',
+            'title' => 'dummy title updated',
+        ])->assertStatus(static::$SUCCESS_RESPONSE);
+
+        // check if post was edited successfully
+        $this->assertDatabaseHas('posts', [
+            'user_id' => $user->id,
+            'description' => 'dummy description updated',
+            'title' => 'dummy title updated',
+        ]);
+    }
+
+    public function test_delete_post(){
+
+        $this->postJson(static::POST_API,[
+            'user_id' => $this->create_user()->id,
+            'description' => 'dummy description',
+            'title' => 'dummy title',
+        ])->assertStatus(static::$SUCCESS_RESPONSE);
+
+        // check database records.
+        $this->assertDatabaseHas('posts', [
+            'user_id' => $user->id,
+            'description' => 'dummy description',
+            'title' => 'dummy title',
+        ]);
+
+        $post = Post::first();
+
+        $this->deleteJson(static::POST_API."/{$post->id}",[])->assertStatus(static::$SUCCESS_RESPONSE);
+    }
+
+    public function create_user(){
+        return User::factory()->create();
     }
 }
